cf766bd8a4fdf66e1417a30e92bc9fbcbc06ace0
Aimini <2026084033@qq.com>
2017-09-24 22:30:44 +0800

    支持括号

--- caculater.py
+++ caculater.py
@@ -12,12 +12,19 @@ university: NCU
 
 - 2017-8-30 02:52:51
 + 整数乘法和除法实现，可以过滤空格
+
+- 2017-09-03 23:44:32
++ 修复上一功能完全不能运行的问题
+
+- 2017-09-24 22:30:21
++ 添加括号支持
 '''
 
 INTEGER = 'INTEGER'
 OPERATOR = 'OPERATOR'
 EOF = 'EOF'
-
+LPAREN = '('
+RPAREN = '('
 
 class Token(object):
     '''标记 类
@@ -43,14 +50,15 @@ class Token(object):
     def __repr__(self):
         return self.__str__()
 
-class Interpreter(object):
+
+class Lexer(object):
     '''解释器 类
     '''
     def __init__(self,text):
         self.text = text
         self.pos = 0
-        self.current_token = None
         self.current_char = self.text[self.pos]
+        
 
     def error(self):
         raise Exception('Error parsing input')
@@ -85,9 +93,7 @@ class Interpreter(object):
         #lexeme是组token的字符序列，如加法token对应的lexeme就是‘+’
         if self.current_char is None:
             return Token(EOF, None)
-
-        if self.current_char.isspace():
-            self.skip_whitespace()
+        self.skip_whitespace()#skip wihte space
 
         if self.current_char.isdigit():
             return Token(INTEGER,self.integer())
@@ -96,25 +102,86 @@ class Interpreter(object):
             temp_token = Token(OPERATOR, self.current_char)
             self.advance()
             return temp_token
+
+        if self.current_char == "(":
+            temp_token = Token(LPAREN,'(')
+            self.advance()
+            return temp_token
+
+        if self.current_char == ")":
+            temp_token = Token(RPAREN,')')
+            self.advance()
+            return temp_token
         self.error()
         
+class Interpreter(object):
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = lexer.get_next_token();
+    def error(self):
+        raise Exception('Invalid syntax')
 
-    def eat(self, token_type):
+    def eatInt(self):
         # 将当前的token的type(self.current_token.type)与传进来的
         # type比较，如果两者相同，当前的token被使用掉(eat)并将下一个token
         # 赋值给current_token；否则抛出一个异常
-        if self.current_token.type == token_type:
-            self.current_token = self.get_next_token()
+        if self.current_token.type == INTEGER:
+            self.current_token = self.lexer.get_next_token()
         else:
             self.error()
+    
+    def eatOp(self,level):
+        '''
+            cosume a opeartor from tokens stream
+        '''
+        if self.current_token.type == OPERATOR:
+            if (level == 0 and self.current_token.value == '^')\
+            or (level == 1 and self.current_token.value in ['*','/'])\
+            or (level == 2 and self.current_token.value in ['+','-']):
+                self.current_token = self.lexer.get_next_token()
+            else:     
+                self.error()
+        else:
+            self.error()
+
+
+    def factor(self):
+        '''factor : INTERGER'''
+        token = self.current_token
+        if token.type == INTEGER:
+            self.eatInt()
+            return token.value
+        if token.type == LPAREN:
+            self.current_token = self.lexer.get_next_token()
+            result = self.expr()
+            self.current_token = self.lexer.get_next_token()
+            return result
+
+    def pwrs(self):
+        '''
+         pwrs: factor((PWR) factor)*
+        '''
+        result = self.factor()
+        while self.current_token.value is '^':
+            self.eatOp(0)
+            result = result ** self.pwrs()
+        return result
+
     def term(self):
         '''
-         获取一个项
-         return an INTEGER token value
+         term: pwrs((MUL | DIV) pwrs)*
         '''
-        token = self.current_token
-        self.eat(INTEGER)
-        return token.value
+        result = self.pwrs()
+        
+        
+        while self.current_token.value in ['*','/']:
+            token = self.current_token
+            self.eatOp(1)
+            if token.value == '*':
+                result = result * self.pwrs()
+            elif token.value == '/':
+                result = result / self.pwrs()
+        return result
 
     def expr(self):
         '''
@@ -122,27 +189,22 @@ class Interpreter(object):
             expr -> INTEGER PLUS INTEGER
             语法分析(parsing)
             识别 token 流中的短语的过程称之为 parsing(语法分析)
+             expr   : term ((PLUS | MINUS) term)*
+            term   : factor ((MUL | DIV) factor)*
+            factor : INTEGER
         '''
         # we expect the current token to be a single-digit integer
-        self.current_token = self.get_next_token()
         result = self.term()
-        while self.current_token.type is not EOF:
-            op = self.current_token
-            if op.type == OPERATOR:
-                self.eat(OPERATOR)
-                # we expect the current token to be a single-digit integer
-                right = self.term()
-                if op.value == '+':
-                    result += right
-                elif op.value == '-':
-                    result -= right
-                elif op.value == '*':
-                    result *= right
-                elif op.value == '/':
-                   result /= right
-                elif op.value == '^':
-                    result = result ** right
         
+        while self.current_token.value in ['-','+']:
+            op = self.current_token
+            self.eatOp(2)
+            # we expect the current token to be a single-digit integer
+            right = self.term()
+            if op.value == '+':
+                result += right
+            elif op.value == '-':
+                result -= right
         return result
 
 def main():
@@ -155,7 +217,8 @@ def main():
             break
         if not text:
             continue
-        interpreter = Interpreter(text)
+        lexer = Lexer(text)
+        interpreter = Interpreter(lexer)
         result = interpreter.expr()
         print(result)
 
